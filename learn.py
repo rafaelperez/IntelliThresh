36b1b22e327266cef6ef31b790bf7bdfbfb18ba8
htcr <congruihetang@gmail.com>
2019-03-08 19:17:45 -0500

    init

---
+++ learn.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+data_root = '/home/mscv1/Desktop/FRL/ChengleiSocial_full/ChengleiSocial/undistorted'
+bg_dir = '000000'
+
+train_set = \
+{
+    '020321': ['400036', '400146'],
+    '020906': ['400036', '400066', '400075']
+}
+
+
